8243754ce7ecec48eb74fcce76ed6499e6766b5d
Ewan bigotte <ewan.bigotte@epitech.eu>
2024-01-31 15:15:46 +0100

    init seasons page
    Add 1st section
    to start: component season

--- pages/seasons.vue
+++ pages/seasons.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Les Saisons</h1>
+  <section>
+    <div class="flex flex-col gap-20 px-36 py-36">
+      <div class="flex justify-between items-center">
+        <div class="flex flex-col w-fit" data-aos="fade-right" data-aos-delay="300">
+          <h1 class="text-6xl">La nature à travers le temps</h1>
+          <hr class="border-2 border-black w-2/3">
+        </div>
+        <div data-aos="fade-left" data-aos-delay="300">
+          <NuxtImg src="/img/mountains/Jura/2.jpg" class="rounded-xl shadow-xl" quality="100" height="200" width="750" />
+        </div>
+      </div>
+      <div class="flex justify-around gap-12 px-8" data-aos="fade-up" data-aos-delay="300">
+        <p class="text-lg text-justify indent-10 w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aut esse
+          ducimus. Qui minima voluptatibus ducimus incidunt hic quo aliquam tempora odio. Excepturi, praesentium
+          perferendis quasi quo nemo quisquam commodi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Architecto id debitis repellat exercitationem eligendi quia adipisci, dicta repellendus impedit praesentium
+          nihil quaerat, odio nostrum aliquid blanditiis deserunt atque possimus eaque? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Praesentium quasi inventore veritatis, dicta animi rerum distinctio saepe nobis
+          nihil aperiam totam aut ullam deleniti libero? Alias dolor voluptatibus veniam fuga? Accusamus labore incidunt
+          consequatur, assumenda repellendus earum saepe dolore. Quam, quibusdam voluptate. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Inventore possimus laudantium nam dolores, cumque voluptas, sit magnam deserunt
+          officia nulla odit molestias velit laborum ab similique reprehenderit quisquam voluptatum est?</p>
+        <p class="text-lg text-justify indent-10 w-1/2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ducimus
+          omnis voluptate, eligendi quasi labore et facilis nemo ex neque numquam dolorum eum, ea tenetur iste velit
+          voluptatibus facere expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minus, quis amet
+          consectetur et veniam, dolore ut mollitia eius ab facilis eveniet beatae, quisquam veritatis quasi animi
+          accusantium unde rerum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis non perspiciatis
+          laboriosam necessitatibus veniam quis deserunt. Labore consectetur ut harum delectus atque laboriosam ad?
+          Atque, deserunt! Iste at natus veritatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          accusantium ducimus a hic optio animi ea sit repudiandae! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Laboriosam praesentium magni eligendi voluptatem possimus at quia ab quidem, ad, necessitatibus id animi
+          assumenda nulla, explicabo pariatur unde vero aut eius.</p>
+      </div>
+    </div>
+  </section>
+
+  <hr class="border-black mx-36 my-4">
+
+  <section>
+    <div class="flex justify-center items-center gap-24">
+      <button class="bg-gray-500/50 w-36 h-12 text-white text-xl font-[telegraph] rounded-full transition-colors duration-200 hover:bg-gray-500/75">Pritemps</button>
+      <button class="bg-gray-500/50 w-36 h-12 text-white text-xl font-[telegraph] rounded-full transition-colors duration-200 hover:bg-gray-500/75">Été</button>
+      <button class="bg-gray-500/50 w-36 h-12 text-white text-xl font-[telegraph] rounded-full transition-colors duration-200 hover:bg-gray-500/75">Automne</button>
+      <button class="bg-gray-500/50 w-36 h-12 text-white text-xl font-[telegraph] rounded-full transition-colors duration-200 hover:bg-gray-500/75">Hiver</button>
+    </div>
+
+    <!-- Compoenent contenant une saisons, a switch avec button -->
+  </section>
+
 </template>
