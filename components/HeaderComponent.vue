e2159e191e6d2939ad10ce00b0e3b4d75739a80d
el_Wawan <bigotte.ewan@gmail.com>
2024-02-08 16:58:10 +0100

    fix loading + Windows adaptation
    fix loading img
    fix Windows mise en page

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="h-[100vh] flex flex-col justify-between bg-cover bg-center transition-all duration-700"
+  <section class="h-[100vh] flex flex-col justify-between bg-cover bg-center transition-all duration-1000"
     :style="{ backgroundImage: `url(${currentImage})` }">
     <header>
       <div class="bg-gradient-to-b from-gray-500 flex pl-10 pt-4">
@@ -8,10 +8,10 @@
           <img src="/img/lightlogo.png" alt="logo" class="w-48">
         </div>
 
-        <div class="mt-6 ml-12 pl-24 w-1/3">
+        <div class="mt-6 pl-24 w-1/3">
           <nav class="flex items-center justify-around flex-1 h-full">
             <NuxtLink to="/"
-              class="bg-white/50 text-white px-4 py-2.5 font-[telegraph] rounded-full transition-color ease-in duration-100 hover:bg-white/75 hover:text-gray-500 hover:transition-color hover:ease-in hover:duration-75">
+              class="bg-white/25 text-white px-4 py-2.5 font-[telegraph] rounded-full transition-color ease-in duration-100 hover:bg-white/50 hover:transition-color hover:ease-in hover:duration-75">
               Accueil</NuxtLink>
             <NuxtLink to="/mountain"
               class="bg-white/25 text-white px-4 py-2.5 font-[telegraph] rounded-full transition-color ease-in duration-100 hover:bg-white/50 hover:transition-color hover:ease-in hover:duration-75">
@@ -28,7 +28,7 @@
       </div>
     </header>
 
-    <div class="text-left w-5/12 ml-24">
+    <div class="text-left w-1/2 ml-24">
       <h1 class="text-8xl text-white font-custom">Les montagnes pour passion</h1>
     </div>
 
@@ -57,7 +57,7 @@ export default {
     },
   },
   mounted() {
-    setInterval(this.changeBackground, 15000);
+    setInterval(this.changeBackground, 10000);
     this.changeBackground();
   },
 }
